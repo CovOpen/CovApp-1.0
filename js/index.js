a56f62e1d223af8e2a82f7198a238e8032e84ffe
alexanderthieme <61984544+alexanderthieme@users.noreply.github.com>
2020-03-12 21:43:10 +0100

    aAdded Copyright remarks

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 /*
-
     This file is part of CovAPP.
 
     CovAPP is free software: you can redistribute it and/or modify
@@ -15,6 +14,7 @@
     You should have received a copy of the GNU General Public License
     along with CovAPP.  If not, see <http://www.gnu.org/licenses/>.
 
+    Copyright 2018-2020 Dr. med. M. Sc. Alexander H. Thieme
     */
 
 function setLanguage(lang) {
