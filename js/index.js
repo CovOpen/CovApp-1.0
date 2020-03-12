8763a7d16248a7c38e21cb7e09bd04ed44706fd0
alexanderthieme <61984544+alexanderthieme@users.noreply.github.com>
2020-03-12 21:26:48 +0100

    aAdded GPL v3 License to all source files

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
+/*
+
+    This file is part of CovAPP.
+
+    CovAPP is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CovAPP is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CovAPP.  If not, see <http://www.gnu.org/licenses/>.
+
+    */
+
 function setLanguage(lang) {
   sessionStorage.setItem('language', lang);
   window.location.href = `./intro/${lang}.html`;
